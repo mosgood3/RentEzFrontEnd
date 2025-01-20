7b8f5639bf17c898112f33bfb4b5cc838b2b5a11
mosgood3 <mattosgood7@gmail.com>
2025-01-20 15:38:45 -0500

    progress

--- src/utils/api/LandlordPost.js
+++ src/utils/api/LandlordPost.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-const BASE_URL = "http://127.0.0.1:8000"; // Your FastAPI server
+const BASE_URL = "http://127.0.0.1:8000";
 
 export const createLandlord = async (data) => {
   try {
     const response = await axios.post(`${BASE_URL}/landlords/signup`, data, {
       headers: {
-        'Content-Type': 'application/json', // Ensure this is set
+        'Content-Type': 'application/json',
       },
     });
     return response.data;
